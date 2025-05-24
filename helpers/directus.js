9ab99f609274cb60b028bf3b2cbbbab734f67c55
znamenskii_aa1 <aznamenskij@primegroup.msk.ru>
2025-05-24 16:54:37 +0400

    token

--- helpers/directus.js
+++ helpers/directus.js
@@ -1,4 +1,4 @@
-export const publicUserToken = 'my5h8azgLjxN36-gCDBVXGN_lIF3SjYi';
+export const publicUserToken = 'aUPKKNMKZ0NZ7YRzOAVfjtc8i34G_rEK';
 const directusAPIUrl = 'https://admin.parkirussia.ru';
 export const getImageURL = (imageId) => `${directusAPIUrl}/assets/${imageId}?access_token=${publicUserToken}&q=100`
 // export const getImageURL = async (imageId) => await directus.request(readAssetRaw(imageId, { quality: 100, access_token: publicUserToken }));
